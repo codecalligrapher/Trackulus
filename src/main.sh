45305bcd6466735f0d4d77f0495cf8c21f4c40ce
Aadidev Sooknanan <a.aadi380@gmail.com>
2024-11-02 14:58:23 -0400

    add logging logic

--- src/main.sh
+++ src/main.sh
@@ -14,17 +14,34 @@ create() {
   fi;
 
   if [[ -z "${PERIODICITY}" ]]; then 
-    PERIODICITY=0; # assume set to zero
+    PERIODICITY=0; # assume set to zero (daily)
+    # TODO: start supporting weekly/monthly periodicities
   fi;
 
   echo "${NAME}" "${PERIODICITY}";
   if [[ ! -e ~/.config/Trackulus/.conf ]]; then
+    echo "Configuration not found, creating in ${HOME}/.config/Trackulus"
     mkdir -p ${HOME}/.config/Trackulus
+    mkdir -p ${HOME}/.config/Trackulus/habits
     touch ${HOME}/.config/Trackulus/habits.conf 
   fi
 
+  touch ${HOME}/.config/Trackulus/habits/${NAME}.txt
+  printf "! name=${NAME}\nperiodicity=${PERIODICITY}\n\n" >> "${HOME}/.config/Trackulus/habits/${NAME}.txt"
+}
 
-  printf "name=${NAME}\nperiodicity=${PERIODICITY}\n\n" >> "${HOME}/.config/Trackulus/habits.conf"
+
+
+log() {
+  while getopts 'n:a:c:' param; do 
+    case "${param}" in 
+      n) NAME=${OPTARG} ;;
+      a) AMOUNT=${OPTARG} ;;
+      c) COMMENT=${OPTARG}
+    esac
+  done;
+  printf "$NAME\n"
+  printf "$(date +%F) : $AMOUNT : ${COMMENT:\t}\n"
 }
 
 "$@"
